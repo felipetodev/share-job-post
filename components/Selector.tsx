b0fd5738bee4656b8c657914e833a23a7a55c88d
felipetodev <fe.ossandon.u@gmail.com>
2023-02-17 00:46:59 -0300

    add vibes input selector

---
+++ components/Selector.tsx
@@ -0,0 +1,76 @@
+import { Fragment, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+import type { Vibes } from '../types'
+
+export const vibes = [
+  { name: 'Professional', icon: '💼' },
+  { name: 'Friendly', icon: '😊' },
+  { name: 'Persuasive', icon: '🧠' },
+  { name: 'Empathetic', icon: '🤗' },
+]
+
+export default function Selector ({ onChange }: any) {
+  const [selected, setSelected] = useState<Vibes>(vibes[0])
+  const handleSelect = (e: Vibes) => {
+    setSelected(e)
+    onChange({ target: { name: 'jobVibe', value: e.name } })
+  }
+  return (
+      <Listbox name="jobVibe" value={selected} onChange={handleSelect}>
+        <div className="relative mt-1">
+          <Listbox.Button className="relative flex items-center w-full text-black cursor-pointer rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+            <span className="block pr-4">{selected.icon}</span>
+            <span className="block truncate">{selected.name}</span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {vibes.map((person, personIdx) => (
+                <Listbox.Option
+                  key={personIdx}
+                  className={({ active }) =>
+                    `relative cursor-pointer select-none py-2 pl-10 pr-4 ${
+                      active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                    }`
+                  }
+                  value={person}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}
+                      >
+                        {person.name}
+                      </span>
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      ) : (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                          {person.icon}
+                        </span>
+                      )}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+  )
+}
