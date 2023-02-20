ff7782a6e51078d8026443ccea6193da71b7a53f
felipetodev <fe.ossandon.u@gmail.com>
2023-02-19 21:58:47 -0300

    replace public env

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,14 +15,14 @@ function MyApp({ Component, pageProps }: AppProps) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.GA_TRACKING_ID}', {
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
                 page_path: window.location.pathname,
               });
             `}
           </Script>
           <Script
             strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
           />
         </>
       )}
